c2afad15a6483251f96fdf941d504deb2344b829
Tharshi Sri <tsrikann@physics.utoronto.ca>
2016-03-02 03:53:54 -0500

    First Commit (Runge Kutta is Finished)

---
+++ ANN_Regression.py
@@ -0,0 +1,6 @@
+class ANNR(object):
+    def __init__(self):
+        # Define Hyperparameters
+        self.inputLayerSize = 2
+        self.outputLayerSize = 1
+        self.
---
+++ README.md
@@ -0,0 +1,1 @@
+# LorentzNN2016
