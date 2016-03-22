Fixed ill fitting behaviour of network in NeuralNet.py...scaling issue

NeuralNet.py
@@ -56,7 +56,7 @@ class BackPropagationNetwork:
                                 (np.vstack([self._layerOutput[-1], np.ones([1, numInputs])]));
                 
             self._layerInput.append(layerInput);
-            self._layerOutput.append(self.sgm(layerInput));
+            self._layerOutput.append(self.act(layerInput));
             
         return self._layerOutput[-1].T
         
@@ -78,11 +78,11 @@ class BackPropagationNetwork:
                 # Compare to the target values
                 outputDelta = self._layerOutput[i] - target.T;
                 error = np.sum(outputDelta**2);
-                delta.append(outputDelta * self.sgm(self._layerInput[i], True));
+                delta.append(outputDelta * self.act(self._layerInput[i], True));
             else:
                 # Compare to the following delta
                 deltaPullback = self.weights[i + 1].T.dot(delta[-1]);
-                delta.append(deltaPullback[:-1, :] * self.sgm(self._layerInput[i], True)); #** check this line **#
+                delta.append(deltaPullback[:-1, :] * self.act(self._layerInput[i], True)); #** check this line **#
         
         # Compute weight*deltas
         for i in range(self.layerCount):
@@ -103,33 +103,54 @@ class BackPropagationNetwork:
                 
         
     # Activation function
-    def sgm(self, z, derivative = False):
+    def act(self, z, derivative = False):
         if not derivative:
             return 1.0/(1.0 + np.exp(-z));
         else:
-            output = self.sgm(z);
+            output = self.act(z);
             return output * (1.0 - output);
             
 #
 # If run as a script, create a test object
 #
 if __name__ == '__main__':
-    print 'Test Neural Network on y = x^2 for -1 < x < 1'
-    bpn = BackPropagationNetwork((1, 3, 3, 1));
+    
+    bpn = BackPropagationNetwork((1, 4, 2, 1));
+     
     print 'Network Structure:\n{0}\n'.format(bpn.shape);
+    print 'Training Via Gradient Descent.'
+    
     # print 'Initial Weights:\n{0}\n'.format(bpn.weights);
     
-    lvInput = np.linspace(-1, 1, 50);
-    lvTarget = lvInput**2;
+    start = -np.pi;
+    end = np.pi;
+    input = np.linspace(start, end, 30);
+    target = np.sin(input**2);
     
-    maxIter = int(10000) + 1;
-    minErr = 1e-5;
-    cost = np.zeros(maxIter);
+    xShift = np.min(input)
+    xNorm = np.max(input) - xShift;
+    
+    yShift = np.min(target);
+    yNorm = np.max(target) - yShift;
+    
+    X = (input - xShift)/xNorm;
+    y = (target - yShift)/yNorm;
+    
+    xTest = np.random.uniform(start, end, 100);
+    yTest = np.sin(xTest**2) + 0.1*np.random.randn(np.size(xTest));
+    
+    xTest = (xTest - xShift)/xNorm;
+    yTest = (yTest - yShift)/yNorm;
+    
+    maxIter = int(100000) + 1;
+    minErr = 1e-4;
+    cost = [];
     
     for i in range(maxIter):
-        err = bpn.trainEpoch(lvInput, lvTarget, 0.01);
-        cost[i] = err;
-        if i % 100000 == 0:
+        err = bpn.trainEpoch(X, y, 0.1);
+        if i % 1e3 == 0:
+            cost.append(err);
+        if i % 1e3 == 0:
             print 'Iteration {0}\tError: {1:0.6f}'.format(i, err);
         if i == maxIter - 1:
             print 'Gradient Descent has reached the maximum number of iterations:'
@@ -137,16 +158,26 @@ if __name__ == '__main__':
         if err <= minErr:
             print 'Minimum error reached at iteration {0}'.format(i);
             break
-    
+    cost = np.array(cost);
     # print 'Final Weights:\n{0}\n'.format(bpn.weights);
     
-    # Display
-    lvOutput = bpn.forward(lvInput);
-    # print 'Input:\n{0}\nOutput:\n{1}\n'.format(lvInput, lvOutput);
+    # Display Results
+    tempX = np.linspace(2*start, 2*end, 1000);
+    tempX = (tempX - xShift)/xNorm;
+    
+    tempY = bpn.forward(tempX);
+    yHat = bpn.forward(X);
+    yHatTest = bpn.forward(xTest);
+    
+    errTest = 0.5*np.sum((yTest - yHatTest)**2)/len(xTest);
+    errTrain = 0.5*np.sum((y - yHat)**2)/len(X);
+    
+    print 'Estimated goodness of fit (training):', errTrain;
+    print 'Estimated goodness of fit (testing):', errTest
     
     fig0 = pl.figure();
-    pl.plot(lvInput, lvOutput);
-    pl.plot(lvInput, lvTarget, '.-');
+    pl.plot(tempX*xNorm + xShift, tempY*yNorm + yShift);
+    pl.plot(input, target, 'ko');
     pl.grid(1);
     
     figCost = pl.figure();
@@ -154,4 +185,10 @@ if __name__ == '__main__':
     pl.grid(1);
     pl.title('Cost Functions');
     pl.xlabel('Iteration');
+    pl.ylabel('Log(c^2 + 1)')
+    
+    figTest = pl.figure();
+    pl.plot(xTest*xNorm + xShift, yTest*yNorm + yShift, 'o');
+    pl.plot(xTest*xNorm + xShift, yHatTest*yNorm + yShift, 'o');
+    
     pl.show();