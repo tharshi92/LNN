trying to fix scaling bug in simulation

lorentzRK.py
@@ -40,21 +40,6 @@ def lorentzSim(T, h, params, x0):
         
         return dxdt, dydt, dzdt;
 
-
-    # define derivative functions
-
-    def x_prime(sigma, x, y, z):
-
-        return -sigma*(x - y);
-
-    def y_prime(rho, x, y, z):
-
-        return x*(rho - z) - y;
-
-    def z_prime(beta, x, y, z):
-
-        return x*y - beta*z;
-
     # integrate using Runge Kutta Method
 
     for i in xrange(0, int(N - 1)):
@@ -88,4 +73,4 @@ def lorentzSim(T, h, params, x0):
 if __name__ == "__main__":
     params = np.array([10.0, 8.0/3.0, 28.0]);
     x0 = np.array([1.0, 1.0, -1.0]);
-    lorentzSim(1.0, 1e-2, params, x0);
+    lorentzSim(600.0, 1e-3, params, x0);

main.py
@@ -9,7 +9,34 @@ import pylab as pl;
 from mpl_toolkits.mplot3d import Axes3D;
 from NeuralNet import BackPropagationNetwork
 
+# helper functions for scaling between 0 and 1
+
+def getScaleParams(x):
+    
+    shift = np.amin(x, axis=0);
+    norm = np.amax(x, axis=0) - shift;
+    scaleParams = [shift, norm];
+    return scaleParams;
+
+def scale(x, scaleParams):
+
+    return (x - scaleParams[0])/scaleParams[1];
+
+def unscale(x, scaleParams):
+    
+    return scaled*scaleParams[1] + scaleParams[0];
+
 # import simulation data
+
+inFile = np.loadtxt('data.txt', skiprows=1);
+data = np.column_stack((inFile[:,0], inFile[:,1], inFile[:,2]));
+signal = inFile[:,5];
+
+sPX = getScaleParams(data);
+sPY = getScaleParams(signal);
+
+X = scale(data, sPX);
+y = scale(signal, sPY);
 
 # train network!
 
@@ -18,26 +45,15 @@ bpn = BackPropagationNetwork((3, 3, 1));
 print 'Network Structure:\n{0}\n'.format(bpn.shape);
 print 'Training Via Gradient Descent.';
 
-data = np.column_stack((x, y, z));
-signal = dZ;
-xShift = np.amin(data, axis=0);
-xNorm = np.amax(data, axis=0) - xShift;
-
-yShift = np.amin(signal, axis=0);
-yNorm = np.amax(signal, axis=0) - yShift;
-
-X = (data - xShift)/xNorm;
-Y = (signal - yShift)/yNorm;
-
-maxIter = int(1e4) + 1;
+maxIter = int(1e5) + 1;
 minErr = 1e-4;
 cost = [];
 
 for i in range(maxIter):
-    err = bpn.trainEpoch(X, Y, 1e-1, 0);
+    err = bpn.trainEpoch(X, y, 1e0, 1e-3);
     if i % 1e3 == 0:
         cost.append(err);
-    if i % 1e3 == 0:
+    if i % 1e4 == 0:
         print 'Iteration {0}\tError: {1:0.6f}'.format(i, err);
     if i == maxIter - 1:
         print 'Gradient Descent has reached the maximum number of iterations:'
@@ -48,11 +64,77 @@ for i in range(maxIter):
         
 cost = np.array(cost);
 yHat = bpn.forward(X);
-errTrain = 0.5*np.sum((Y - yHat)**2)/len(X);
+errTrain = 0.5*np.sum((y - yHat)**2)/len(X);
 
 print 'Final Cost (training):', errTrain;
 
 # rerun simulation with trained network
+
+# define simulation parameters
+T = 1.0;    # simultion length
+h = 1e-2;    # timestep
+N = T/h;   # number of steps
+
+params = np.array([10.0, 8.0/3.0, 28.0]);
+x0 = np.array([1.0, 1.0, -1.0]);
+
+x = np.zeros(N);
+y = np.zeros(N);
+z = np.zeros(N);
+dX = np.zeros(N);
+dY = np.zeros(N);
+dZ = np.zeros(N);
+
+# parameters for the lorentz model
+sigma = params[0];
+beta = params[1];
+rho = params[2];
+
+# initial conditions
+x[0] = x0[0];
+y[0] = x0[1];
+z[0] = x0[2];
+
+# define dynamical equation
+
+def dynamics(x, y, z, sigma, beta, rho):
+    
+    dxdt = -sigma*(x - y);
+    
+    dydt = x*(rho - z) - y;
+    
+    dzdt = x*y - beta*z;
+    
+    return dxdt, dydt;
+
+# integrate using Runge Kutta Method
+
+for i in xrange(0, int(N - 1)):
+
+    p1, q1 = dynamics(x[i], y[i], z[i], sigma, beta, rho);
+    temp1 = scale(np.column_stack((x[i], y[i], z[i])), sPX);
+    r1 = bpn.forward(temp1);
+                     
+    dX[i] = p1;
+    dY[i] = q1;
+    dZ[i] = r1;
+
+    p2, q2 = dynamics(x[i] + h*p1/2.0, y[i] + h*q1/2.0, z[i] + h*r1/2.0,\
+                      sigma, beta, rho);
+    temp2 = np.scale(np.column_stack((x[i] + h*p1/2.0, y[i] + h*q1/2.0, z[i] + h*r1/2.0)), sPX);
+    r2 = bpn.forward(temp2);
+                     
+    p3, q3= dynamics(x[i] + h*p2/2.0, y[i] + h*q2/2.0, z[i] + h*r2/2.0,\
+                     sigma, beta, rho);
+    r3 = bpn.forward(np.column_stack((x[i] + h*p2/2.0, y[i] + h*q2/2.0, z[i] + h*r2/2.0)));
+                     
+    p4, q4 = dynamics(x[i] + h*p3, y[i] + h*q3, z[i] + h*r3,\
+                      sigma, beta, rho);
+    r4 = bpn.forward(np.column_stack((x[i] + h*p3, y[i] + h*q3, z[i] + h*r3)));
+
+    x[i+1] = x[i] + h*(p1 + 2.0*p2 + 2.0*p3 + p4)/6.0;
+    y[i+1] = y[i] + h*(q1 + 2.0*q2 + 2.0*q3 + q4)/6.0;
+    z[i+1] = z[i] + h*(r1 + 2.0*r2 + 2.0*r3 + r4)/6.0;
 
 ##fig0 = pl.figure();
 ##pl.plot(tempX*xNorm + xShift, tempY*yNorm + yShift, label = 'fit');