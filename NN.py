41e18ef8d90d6057ae0ec182bcd9c5bebe2d7af3
Tharshi Sri <tsrikann@physics.utoronto.ca>
2016-04-02 00:47:47 -0400

    fixed NN.py to work with lorentz data set.

--- NN.py
+++ NN.py
@@ -1,11 +1,15 @@
 # 3 layer Neural network attempts to fit training data
 # Coded by Tharshi Srikannathasan tsrikann@physics.utoronto.ca
 #
-# Version 1 (0216)
-#     3 Layer Net with regularization and minimization via python.optimize 'BFGS' method
-#     Problems: Fitting functions that have concavity similar to even polynomials
-# Version 2 (031516)
-#     Added bias weights
+## Version 1 (0216)
+##     3 Layer Net with regularization and minimization via python.optimize 'BFGS' method
+##     Problems: Fitting functions that have concavity similar to even polynomials
+## Version 2 (0316)
+##     Fixed scaling issue for fitting curves
+## Version 3 (040116)
+##     Added Lorentz Model (BFGS Convergence is much faster than gradient descent with momentum)
+##     no test data as of yet
+##     regularization exists but is not used
 
 
 import numpy as np
@@ -196,41 +200,50 @@ if __name__ == '__main__':
     # Options
     plot = 1            # turn on plots of cost and fit
     gradTest = 0;       # check if the gradient implementation from NN.py works by checking with a numerical gradient
-    regression = 0;     # 1 -> regression, 0 -> simple classification
+    regression = 1;     # 1 -> regression, 0 -> simple classification
     
     if regression:
     
         # create data 
     
-        sigErr = 0;
+        # helper functions for scaling between 0 and 1
 
-        X = np.linspace(-1, 1, 10);
-        X = np.reshape(X, (len(X), 1));
-        y = np.log(X**2 + 1) + sigErr*np.random.randn(len(X), 1);
+        def getScaleParams(x):
+            
+            shift = np.amin(x, axis=0);
+            norm = np.amax(x, axis=0) - shift;
+            scaleParams = [shift, norm];
+            return scaleParams;
 
-        testX = np.random.uniform(-1, 1, 10);
-        testX = np.reshape(testX, (len(testX), 1));
-        testY = np.log(testX**2 + 1) + sigErr*np.random.randn(len(testX), 1);
+        def scale(x, scaleParams):
 
-        # print X.shape, type(X)
-        # print testX.shape, type(testX)
-        # print y.shape, type(y)
-        # print testY.shape, type(testY)
-    
-        # Normalize Data
-        xScale = np.amax(np.abs(X), axis=0);
-        yScale = np.amax(np.abs(y), axis=0);
+            return (x - scaleParams[0])/scaleParams[1];
 
-        X = X/xScale;
-        y = y/yScale;
-        testX = testX/xScale;
-        testY = testY/yScale;
+        def unscale(x, scaleParams):
+            
+            return x*scaleParams[1] + scaleParams[0];
+
+        # import simulation data
+
+        inFile = np.loadtxt('data.txt', skiprows=1);
+        data = np.column_stack((inFile[:,0], inFile[:,1], inFile[:,2]));
+        signal = inFile[:,5];
+
+        sPX = getScaleParams(data);
+        sPY = getScaleParams(signal);
+
+        X = scale(data, sPX);
+        y = scale(signal, sPY);
+        y = np.reshape(y, (len(y), 1));
+
+        testX = X;
+        testY = y;
 
         # Network Parameters
-        dimX = len(X.T);     # dimension of input data
-        dimY = len(y.T);     # dimenstion of output data
-        n = 500;              # number of neurons in hidden layer
-        regParam = 1e-5;     # regularization hyperparameter
+        dimX = X.shape[1];     # dimension of input data
+        dimY = y.shape[1];     # dimenstion of output data
+        n = 5;              # number of neurons in hidden layer
+        regParam = 0;     # regularization hyperparameter
 
         # create network
         NN = Neural_Network(dimX, dimY, n, regParam);
@@ -260,37 +273,147 @@ if __name__ == '__main__':
             print '-----------------------------------------------------'
             print ''
 
-        if plot:
+        pl.figure();
+        pl.plot(T.J, label='Training Cost')
+        pl.grid(1)
+        pl.plot(T.testJ, label='Testing Cost')
+        pl.xlabel('iteration')
+        pl.title('Cost Functions')
+        pl.legend()
 
-            pl.figure();
-            pl.plot(T.J, label='Training Cost')
-            pl.grid(1)
-            pl.plot(T.testJ, label='Testing Cost')
-            pl.xlabel('iteration')
-            pl.title('Cost Functions')
-            pl.legend()
+            # rerun simulation with trained network
 
-            # test network for various combinations
-            numPoints = 100;
-            x0 = np.linspace(-xScale[0], xScale[0], numPoints);
-            x0 = np.reshape(x0, (len(x0), 1));
+        # define simulation parameters
+        T = 8.0;    # simultion length
+        h = 1e-3;    # timestep
+        N = T/h;   # number of steps
 
-            # normalize data (same way training data was normalized)
-            x0 = x0/xScale;
+        params = np.array([10.0, 8.0/3.0, 28.0]);
+        x0 = np.array([1.508870, -1.531271, 25.46091]);
 
-            # forward prop through network with trained weights
-            fit = NN.forward(x0);
+        x = np.zeros(N);
+        y = np.zeros(N);
+        z = np.zeros(N);
+        dX = np.zeros(N);
+        dY = np.zeros(N);
+        dZ = np.zeros(N);
 
-            pl.figure();
-            pl.scatter(xScale[0]*X[:, 0], yScale*y, c='b', marker = 'o');
-            pl.scatter(xScale[0]*testX[:,0], yScale*testY, c='r', marker = 'o');
-            pl.plot(x0*xScale, yScale*fit);
-            pl.xlabel('x0');
-            pl.ylabel('y');
-            pl.title('Fit of Data')
-            print ''
+        xt = np.zeros(N);
+        yt = np.zeros(N);
+        zt = np.zeros(N);
+        dXt = np.zeros(N);
+        dYt = np.zeros(N);
+        dZt = np.zeros(N);
 
-            pl.show();
+        # parameters for the lorentz model
+        sigma = params[0];
+        beta = params[1];
+        rho = params[2];
+
+        # initial conditions
+        x[0] = x0[0];
+        y[0] = x0[1];
+        z[0] = x0[2];
+        xt[0] = x0[0];
+        yt[0] = x0[1];
+        zt[0] = x0[2];
+
+        # define dynamical equation
+
+        def dynamics(x, y, z, sigma, beta, rho):
+            
+            dxdt = -sigma*(x - y);
+            
+            dydt = x*(rho - z) - y;
+            
+            dzdt = x*y - beta*z;
+            
+            return dxdt, dydt, dzdt;
+
+        # integrate using Runge Kutta Method
+
+        for i in xrange(0, int(N - 1)):
+
+            # update nn simulation
+
+            p1, q1, dummy = dynamics(x[i], y[i], z[i], sigma, beta, rho);
+            temp1 = scale(np.column_stack((x[i], y[i], z[i])), sPX);
+            r1 = NN.forward(temp1);
+            r1 = unscale(r1, sPY);
+                             
+            dX[i] = p1;
+            dY[i] = q1;
+            dZ[i] = r1;
+
+            p2, q2, dummy = dynamics(x[i] + h*p1/2.0, y[i] + h*q1/2.0, z[i] + h*r1/2.0,\
+                              sigma, beta, rho);
+            temp2 = scale(np.column_stack((x[i] + h*p1/2.0, y[i] + h*q1/2.0, z[i] + h*r1/2.0)), sPX);
+            r2 = NN.forward(temp2);
+            r2 = unscale(r2, sPY);
+                             
+            p3, q3, dummy = dynamics(x[i] + h*p2/2.0, y[i] + h*q2/2.0, z[i] + h*r2/2.0,\
+                             sigma, beta, rho);
+            temp3 = scale(np.column_stack((x[i] + h*p2/2.0, y[i] + h*q2/2.0, z[i] + h*r2/2.0)), sPX);
+            r3 = NN.forward(temp3);
+            r3 = unscale(r3, sPY);
+                             
+            p4, q4, dummy = dynamics(x[i] + h*p3, y[i] + h*q3, z[i] + h*r3,\
+                              sigma, beta, rho);
+            temp4 = scale(np.column_stack((x[i] + h*p3, y[i] + h*q3, z[i] + h*r3)), sPX);
+            r4 = NN.forward(temp4);
+            r4 = unscale(r4, sPY);
+
+            x[i+1] = x[i] + h*(p1 + 2.0*p2 + 2.0*p3 + p4)/6.0;
+            y[i+1] = y[i] + h*(q1 + 2.0*q2 + 2.0*q3 + q4)/6.0;
+            z[i+1] = z[i] + h*(r1 + 2.0*r2 + 2.0*r3 + r4)/6.0;
+
+            # update truth
+            
+            p1t, q1t, r1t = dynamics(xt[i], yt[i], zt[i], sigma, beta, rho);
+            dXt[i] = p1t;
+            dYt[i] = q1t;
+            dZt[i] = r1t;
+
+            p2t, q2t, r2t = dynamics(xt[i] + h*p1t/2.0, yt[i] + h*q1t/2.0, zt[i] + h*r1t/2.0,\
+                                     sigma, beta, rho);
+            p3t, q3t, r3t = dynamics(xt[i] + h*p2t/2.0, yt[i] + h*q2t/2.0, zt[i] + h*r2t/2.0,\
+                                     sigma, beta, rho);
+            p4t, q4t, r4t = dynamics(xt[i] + h*p3t, yt[i] + h*q3t, zt[i] + h*r3t,\
+                                     sigma, beta, rho);
+
+            xt[i+1] = xt[i] + h*(p1t + 2.0*p2t + 2.0*p3t + p4t)/6.0;
+            yt[i+1] = yt[i] + h*(q1t + 2.0*q2t + 2.0*q3t + q4t)/6.0;
+            zt[i+1] = zt[i] + h*(r1t + 2.0*r2t + 2.0*r3t + r4t)/6.0;
+
+        fig1 = pl.figure();
+        pl.plot(x, 'g', label='x');
+        pl.plot(y, 'r', label='y');
+        pl.plot(z, 'k', label='z');
+        pl.plot(xt, 'c', label='x truth');
+        pl.plot(yt, 'm', label='y truth');
+        pl.plot(zt, '0.75', label='z truth');
+        pl.legend();
+        pl.xlabel("timestep");
+
+        ##fig2 = pl.figure();
+        ##ax = fig2.add_subplot(111, projection='3d');
+        ##ax.scatter(x, y, z, marker = 'o', color = 'r');
+        ##ax.scatter(xt, yt, zt, marker = 'o', color = 'b');
+        ##ax.set_xlabel('X');
+        ##ax.set_ylabel('Y');
+        ##ax.set_zlabel('Z');
+
+
+        fig3 = pl.figure();
+        pl.plot((x - xt), label='x error');
+        pl.plot((y - yt), label='y error');
+        pl.plot((z - zt), label='z error');
+        pl.title('errors');
+        pl.legend();
+        pl.xlabel("timestep");
+
+
+        pl.show();
             
     else:
         
